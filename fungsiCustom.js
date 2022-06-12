f2b9f9ddece9d97d18b7bb1eef113b783e573ece
syaddadh1211 <syaddad.bahalwan@realworld-systems.com>
2022-06-12 21:53:01 +0700

    update fungsiCustom.js

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,34 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (err, data) => {
+  let jsonAll = [];
+  let jsonTest = "";
+
+  fs.readFile(file1, "utf8", (err, data1) => {
+    if (err) {
+      return err;
+    }
+    fs.readFile(file2, "utf8", (err, data2) => {
+      if (err) {
+        return err;
+      }
+      fs.readFile(file3, "utf8", (err, data3) => {
+        if (err) {
+          return err;
+        }
+        jsonTest = JSON.parse(data1);
+        jsonAll.push(jsonTest.message.split(" ")[1]);
+        jsonTest = JSON.parse(data2);
+        jsonAll.push(jsonTest[0].message.split(" ")[1]);
+        jsonTest = JSON.parse(data3);
+        jsonAll.push(jsonTest[0].data.message.split(" ")[1]);
+      });
+    });
+  });
+  console.log(data);
+  return null, data;
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
